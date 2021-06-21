a9b5568683d819fa1d2821f0026e3d5650bb96af
johnsci911 <johnguitarizta@gmail.com>
2021-06-21 23:52:36 +0800

    Make progress bar reactive

--- resources/views/channels/upload.blade.php
+++ resources/views/channels/upload.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container-fluid">
 	<div class="row justify-content-center">
 		<channel-uploads :channel="{{ $channel }}" inline-template>
 			<div class="col-md-8">
@@ -12,9 +12,9 @@
                 </div>
 
 				<div class="card p-3" v-else>
-					<div class="my-4">
+					<div class="my-4" v-for="video in videos">
 						<div class="progress mb-3">
-							<div class="progress-bar progress-bar-striped progress-bar-animated " role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
+							<div class="progress-bar progress-bar-striped progress-bar-animated " role="progressbar" :style="{ width: `${progress[video.name]}%` }" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
 						    </div>
 					    </div>
                         <div class="row">
@@ -25,7 +25,7 @@
                             </div>
                             <div class="col-md-4">
                                 <h4 class="text-center">
-                                    My Awesome video
+                                    @{{ video.name }}
                                 </h4>
                             </div>
                         </div>
