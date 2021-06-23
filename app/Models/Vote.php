9cadf3927e6ddeb1a80d4f80179ee745cfbd19ac
johnsci911 <johnguitarizta@gmail.com>
2021-06-24 03:54:59 +0800

    Reusable vote components part1

--- app/Models/Vote.php
+++ app/Models/Vote.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Models;
-
 class Vote extends Model
 {
     public function voteable()
--- resources/js/app.js
+++ resources/js/app.js
@@ -21,6 +21,7 @@ window.Vue = require('vue').default;
 
 Vue.config.ignoredElements = ['video-js'];
 Vue.component('subscribe-button', require('./components/subscribe-button.vue').default)
+Vue.component('votes', require('./components/votes.vue').default)
 require('./components/channel-uploads');
 
 /**
