21b7adf9f208ee4ae8ed873632be4921c75b043c
johnsci911 <johnguitarizta@gmail.com>
2021-06-24 22:36:58 +0800

    Remove welcome.blade.php and update home route

--- routes/web.php
+++ routes/web.php
@@ -19,13 +19,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::resource('channels', ChannelController::class);
 
 Route::get('videos/{video}', [VideoController::class, 'show']);
